4eb7e95da329de81eba1f28c5b783707f58d0927
Bruno <seletonurb@gmail.com>
2018-07-26 17:37:16 -0300

    Change the Search Bar component state by changing input value

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: "" };
+    console.log(this.state);
+  }
+
   render() {
-    return <input onChange={this.onInputChange} />;
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value }) } />
+        Value of the input: {this.state.term}
+      </div>
+    );
   }
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+
 }
 
 export default SearchBar;
