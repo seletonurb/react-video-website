a940a27b455d82202c53f9e6b321e2513649b1fd
Bruno <seletonurb@gmail.com>
2018-07-26 22:10:00 -0300

    Refactor App from Function Component to Class Component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
 
-import SearchBar from './components/search_bar';
+import SearchBar from "./components/search_bar";
 
 const YOUTUBE_API_KEY = "AIzaSyCJLgy2AdFKjVokCQ00MD-VD7AumXjCJuk";
 
-YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-// Create a new component. This component should produce some HTML
-const App = function() {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+    this.state = { videos: [] };
+
+    YTSearch({ key: YOUTUBE_API_KEY, term: "surfboards" }, videos => {
+      this.setState({ videos }); // this.setState({ videos: videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
 // Take this component's generated HTML and put it n the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector(".container"));
