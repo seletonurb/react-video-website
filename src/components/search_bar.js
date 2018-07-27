108ffdd5a93fa1eba6c11d12a51bb5434e78f22a
Bruno <seletonurb@gmail.com>
2018-07-27 14:15:29 -0300

    Propagate search video term callback function from parent component down to Search Bar (child) component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -12,9 +12,14 @@ class SearchBar extends Component {
         <input
         value={this.state.term}
         placeholder={this.state.placeholder}
-        onChange={event => this.setState({ term: event.target.value }) } />
+        onChange={event => this.onInputChange(event.target.value) } />
       </div>
     );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 
 }
--- src/index.js
+++ src/index.js
@@ -16,16 +16,22 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     };
+    this.videoSearch("surfboards");
+  }
 
-    YTSearch({ key: YOUTUBE_API_KEY, term: "surfboards" }, videos => {
-      this.setState({ videos: videos, selectedVideo: videos[0] }); // this.setState({ videos: videos });
+  videoSearch(term) {
+    YTSearch({ key: YOUTUBE_API_KEY, term: term }, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
